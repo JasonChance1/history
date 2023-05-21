9851c2e800b295ab16c8927c9d8a9d3a9be2d5da
admin <3136966811@qq.com>
2023-05-21 14:09:13 +0800

    分享

--- app/src/main/java/com/example/history/DetailChineseHistoryActivity.java
+++ app/src/main/java/com/example/history/DetailChineseHistoryActivity.java
@@ -8,6 +8,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.history.bean.DynastyContent;
@@ -15,21 +17,47 @@ import com.example.history.bean.MySqliteOpenHelper;
 import com.mysql.fabric.xmlrpc.base.Data;
 
 public class DetailChineseHistoryActivity extends AppCompatActivity {
-    private TextView content;
-
+    private TextView content,shareContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_chinese_history);
 
-        content=findViewById(R.id.detail_content_tv);
+        initView();
+        initData();
+        bindEvent();
+    }
 
+    private void initData(){
         Intent intent=getIntent();
 
         String s =intent.getStringExtra("data");
         Log.d("DetailChinese",s);
         content.setText(s);
+    }
+
+    private void initView(){
+        content=findViewById(R.id.detail_content_tv);
+        shareContent = findViewById(R.id.share_content);
+    }
+
+    private void bindEvent(){
+        shareContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(() -> {
+                    allShare("我的分享"+content.getText().toString()); // 不传递文件参数
+                }).start();
+            }
+        });
+    }
 
 
+    public void allShare(String share) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, share);
+        shareIntent = Intent.createChooser(shareIntent, share);
+        startActivity(shareIntent);
     }
 }
